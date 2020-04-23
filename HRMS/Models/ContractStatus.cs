a9f23cabd851bf142935e09cf7f9447e865a080c
Dawid Koral <s15153@pjwstk.edu.pl>
2020-04-23 23:25:23 +0200

    Add project files.

---
+++ HRMS/Models/ContractStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRMS.Models
+{
+    public partial class ContractStatus
+    {
+        public ContractStatus()
+        {
+            Contract = new HashSet<Contract>();
+        }
+
+        public int IdContractStatus { get; set; }
+        public string StatusName { get; set; }
+
+        public virtual ICollection<Contract> Contract { get; set; }
+    }
+}
---
+++ HRMS/Models/RequestStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRMS.Models
+{
+    public partial class RequestStatus
+    {
+        public RequestStatus()
+        {
+            Request = new HashSet<Request>();
+        }
+
+        public int IdRequestStatus { get; set; }
+        public string StatusName { get; set; }
+
+        public virtual ICollection<Request> Request { get; set; }
+    }
+}
